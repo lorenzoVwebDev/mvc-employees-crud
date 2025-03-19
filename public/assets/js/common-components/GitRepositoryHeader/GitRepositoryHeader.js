d3918ccb76463f0dcb5014ddfce2e973a69649cc
lorenzoVwebDev <lorenzoviganego.work@libero.it>
2025-03-19 16:57:50 +0100

    Updated

--- public/assets/js/common-components/GitRepositoryHeader/GitRepositoryHeader.js
+++ public/assets/js/common-components/GitRepositoryHeader/GitRepositoryHeader.js
@@ -1,5 +1,5 @@
 import { url } from '../../utils/globalVariables.js'
 
-let  GitRepositoryHeader =  `<header class="git-repository-header">Git repository 👉🏻<a href="https://github.com/lorenzoVwebDev/mvc-error-handling.git">MVC-error-handling</a></header>`;
+let  GitRepositoryHeader =  `<header class="git-repository-header">Git repository 👉🏻<a href="https://github.com/lorenzoVwebDev/mvc-employees-crud">mvc-empoloyees-crud</a></header>`;
 
 export default GitRepositoryHeader;
